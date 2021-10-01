c3cfe578e40a63d4ff3433016f71731a5e0ae34b
campoiratxe <iratxe.campo@alumni.mondragon.edu>
2021-10-01 16:50:33 +0200

    client routes cambiado

--- flask_app/monolithic/application/client/routes_client.py
+++ flask_app/monolithic/application/client/routes_client.py
@@ -1,6 +1,6 @@
 from flask import current_app as app
 from flask import request, jsonify, abort
-from werkzeug.exceptions import BadRequest, UnsupportedMediaType
+from werkzeug.exceptions import BadRequest, UnsupportedMediaType, NotFound
 
 from .model_client import Client
 from .. import Session
@@ -26,5 +26,25 @@ def create_client():
         session.close()
         abort(BadRequest.code)
     response = jsonify(new_client.as_dict())
+    session.close()
+    return response
+
+@app.route('/clients', methods=['GET'])
+def view_clients():
+    session = Session()
+    print("GET All Clients.")
+    clients = session.query(Client).all()
+    response = jsonify(Client.list_as_dict(clients))
+    session.close()
+    return response
+
+@app.route('/client/<int:client_id>', methods=['GET'])
+def view_client(client_id):
+    session = Session()
+    client = session.query(Client).get(client_id)
+    if not client:
+        abort(NotFound.code)
+    print("GET Client {}: {}".format(client_id, client))
+    response = jsonify(client.as_dict())
     session.close()
     return response
