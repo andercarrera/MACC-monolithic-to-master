c8277a470987df8b6eb2fa2173a38881f368a0e9
Ander <andercarrera@hotmail.com>
2021-10-18 16:03:37 +0200

    Modify docker-compose.yml

--- flask_app/client/app.py
+++ flask_app/client/app.py
@@ -5,3 +5,5 @@ app.app_context().push()
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=13000)
+
+# PYTHONUNBUFFERED=1;SQLALCHEMY_DATABASE_URI=sqlite:///monolithic.db?check_same_thread=False;SQLALCHEMY_TRACK_MODIFICATIONS=False
