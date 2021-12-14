c1687e46d59adfa2165aa45391463f8d8eada6be
Ander <andercarrera@hotmail.com>
2021-12-14 21:39:52 +0100

    Replace hardcoded REST requests to via-Consul requests

--- flask_app/order/application/auth.py
+++ flask_app/order/application/auth.py
@@ -1,3 +1,4 @@
+import json
 from datetime import datetime
 from time import sleep
 
@@ -6,9 +7,7 @@ import requests
 from jwt import InvalidSignatureError
 from werkzeug.exceptions import Unauthorized, abort, Forbidden
 
-from . import Config, publisher_order
-
-base_url_client = "http://{}:{}/".format(Config.CLIENT_IP, Config.GUNICORN_PORT)
+from . import Config, BLConsul
 
 
 class RsaSingleton(object):
@@ -21,11 +20,12 @@ class RsaSingleton(object):
     @staticmethod
     def request_public_key():
         while RsaSingleton.public_key is None:
-            try:
-                response = requests.get(str(base_url_client + 'client/get_public_key'), verify=False).json()
-                RsaSingleton.public_key = response['public_key']
-                publisher_order.publish_msg("event_exchange", "auth.certificate", "New certificate created for Order")
-            except:
+            ret_message, status_code = external_service_response("client", "get_public_key")
+            if status_code == 200:
+                json_tree = json.loads(ret_message)
+                resp = json.loads(json_tree["response"])
+                RsaSingleton.public_key = resp["public_key"]
+            else:
                 print('Order waiting for public key', flush=True)
                 sleep(3)
 
@@ -51,3 +51,41 @@ class RsaSingleton(object):
                 abort(Forbidden.code, "JWT Token expired")
         except InvalidSignatureError:
             abort(Unauthorized.code, "JWT signature verification failed")
+
+
+# Consul external ####################################################################################################
+def external_service_response(external_service_name, path):
+    service = BLConsul.get_instance().get_service(external_service_name)
+    service['Name'] = external_service_name
+    if service['Address'] is None or service['Port'] is None:
+        ret_message = "The service does not exist or there is no healthy replica"
+        status_code = 404
+    else:
+        service['Path'] = path
+        ret_message, status_code = call_external_service(service)
+    return ret_message, status_code
+
+
+def call_external_service(service):
+    config = Config.get_instance()
+    url = "http://{host}:{port}/{service}/{path}".format(
+        host=service['Address'],
+        port=service['Port'],
+        service=service['Name'],
+        path=service['Path']
+    )
+    response = requests.get(url)
+    if response:
+        ret_message = json.dumps({
+            "caller": config.SERVICE_NAME,
+            "callerURL": "{}:{}".format(config.IP, config.PORT),
+            "answerer": service['Name'],
+            "answererURL": "{}:{}".format(service['Address'], service['Port']),
+            "response": response.text,
+            "status_code": response.status_code
+        })
+        status_code = response.status_code
+    else:
+        ret_message = "Could not get message"
+        status_code = 500
+    return ret_message, status_code
