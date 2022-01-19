c639128cb8b1f8b3361bb820c460a5e4fb0f7f28
Ander <andercarrera@hotmail.com>
2022-01-19 16:25:01 +0100

    Remove haproxy related files

--- start-dev.sh
+++ start-dev.sh
@@ -3,10 +3,6 @@ docker-compose down -t0
 
 docker-compose up -d consul
 sleep 10s
-docker-compose up -d haproxy
-sleep 10s
-docker-compose up -d rabbitmq
-sleep 10s
 docker-compose up -d client
 sleep 5s
 docker-compose up
